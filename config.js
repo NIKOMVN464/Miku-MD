90edece45476c43f84f1adea07f871fa1adc8c07
NIKOMVN464 <113065354+NIKOMVN464@users.noreply.github.com>
2022-09-21 11:19:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['22585134712']
+global.OwnerNumber = ['22585134712']
+global.ownertag = ['22585134712']
+global.BotName = "NIKO"
+global.packname = "Niko bot"
+global.author = "By: NIKO"
+global.OwnerName = "NIKO"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
@@ -24,8 +24,8 @@ global.prefa = ['-']
 
 
 global.location = "West Bengal, India"
-global.reactmoji = "❤️"
-global.themeemoji = "💖"
+global.reactmoji = "☃️"
+global.themeemoji = "🔥"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/FantoX001"
 global.lolhuman = "KaysaS"
